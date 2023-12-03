bf9516d3f6f59742c60eddaef21ba7ac7f0b8f18
Shashikant <58462785+SHASHIKANT-A@users.noreply.github.com>
2023-12-03 08:20:21 +0530

    first functionality added
    abc

---
+++ tute1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+//this program is created by Shashikant
+/*This is
+multiline comment*/
+int main(){
+    cout<<"Hello world";
+    return 0;
+}
